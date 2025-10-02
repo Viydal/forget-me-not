0b4b7447391e5d0f71129952deccfe2463b7def8
rory <a1886526@adelaide.edu.au>
2025-10-02 17:52:31 +0930

    tiles

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,42 +1,42 @@
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-class GameManager : MonoBehaviour
-{
-    PlayerRecorder playerRecorder;
-    GameObject shadowPrefab;
-    Transform spawnPoint;
+// class GameManager : MonoBehaviour
+// {
+//     PlayerRecorder playerRecorder;
+//     GameObject shadowPrefab;
+//     Transform spawnPoint;
 
-    void Start()
-    {
-        StartNewLife();
-    }
+//     void Start()
+//     {
+//         StartNewLife();
+//     }
 
-    void StartNewLife()
-    {
-        playerRecorder.StartRecording();
-        RespawnPlayer();
-    }
+//     void StartNewLife()
+//     {
+//         playerRecorder.StartRecording();
+//         RespawnPlayer();
+//     }
 
-    void OnPlayerDeath()
-    {
-        // Stop recording and create a shadow
-        List<InputFrame> data = playerRecorder.StopRecording();
-        SpawnShadow(data);
+//     void OnPlayerDeath()
+//     {
+//         // Stop recording and create a shadow
+//         List<InputFrame> data = playerRecorder.StopRecording();
+//         SpawnShadow(data);
 
-        // Start new life
-        StartNewLife();
-    }
+//         // Start new life
+//         StartNewLife();
+//     }
 
-    void SpawnShadow(List<InputFrame> data)
-    {
-        GameObject shadow = Instantiate(shadowPrefab, spawnPoint.position, Quaternion.identity);
-        shadow.GetComponent<ShadowReplayer>().Init(data);
-    }
+//     void SpawnShadow(List<InputFrame> data)
+//     {
+//         GameObject shadow = Instantiate(shadowPrefab, spawnPoint.position, Quaternion.identity);
+//         shadow.GetComponent<ShadowReplayer>().Init(data);
+//     }
 
-    void RespawnPlayer()
-    {
-        // Instantiate player at spawn point
-        Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
-    }
-}
+//     void RespawnPlayer()
+//     {
+//         // Instantiate player at spawn point
+//         Instantiate(playerPrefab, spawnPoint.position, Quaternion.identity);
+//     }
+// }
