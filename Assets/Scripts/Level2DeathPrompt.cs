ca98313a5a95a9b3f38e473a0e35f1af41a3eeeb
rory <a1886526@adelaide.edu.au>
2025-10-06 05:00:50 +1030

    butt

---
+++ Assets/Scripts/Level2DeathPrompt.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Level2DeathPrompt : MonoBehaviour
+{
+    [SerializeField] private GameObject popUpPrefab;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (GameManager.Instance.firstDoorInteraction)
+            {
+                GameManager.Instance.firstDoorInteraction = false;
+                GameObject popUpObject = Instantiate(popUpPrefab, new Vector3(0, 0, 0), new Quaternion());
+                popUpObject.GetComponent<PopUp>().text_value = "Press R to Respawn.";
+            }
+        }
+    }
+}
